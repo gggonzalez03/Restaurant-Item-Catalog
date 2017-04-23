6e1b70241f41d9a6ab8cef0d6525f584a3c89d8d
ginggonzalez03 <ginggonzalez3@gmail.com>
2017-04-23 14:07:06 -0700

    fix: Lag before menu list is rendered
    The previously viewed menu shows for a second and then renders the new menu
    being viewed. In some cases, this could be slower and the user might see false
    information on the menu list. Because of that, the menu should be blank before
    the new menu list is rendered on the screen.

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,7 @@ angular.module('catalog', [])
     $scope.is_owned;
 
     catalog.get_menu_by_restaurant = function(restaurant_id){
+        $scope.restaurant_menu = "";
     	$http.get("/restaurant/menu/" + restaurant_id)
     	.then(function(data){
     	    $scope.restaurant_menu = data.data.results;
@@ -12,6 +13,7 @@ angular.module('catalog', [])
     };
 
     catalog.is_item_owned = function(item_id){
+        $scope.is_owned = "";
     	$http.get("/restaurant/menu/" + item_id + "/isowned")
     	.then(function(data){
     	    $scope.is_owned = data.data.results[0].answer;
